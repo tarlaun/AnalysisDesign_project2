a69a520f82565922002085e9659939046f361bc5
MohammadMahdi Jarrahi <jarrahi.mm@gmail.com>
2021-11-17 12:00:19 +0330

    Create Views of Expertise Orders List With Demo Data #48

--- docicome/account/urls.py
+++ docicome/account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('signup/', views.register, name='signup'),
+    # پزشک :: نمایش درخواست‌های فعال موجود برای تخصص پزشک
+    # پزشک :: قابلیت قبول درخواست
+    path('expertise_orders_list/', views.expertise_orders_list, name='expertise_orders_list'),
+    path('accept_order/<int:order_id>/', views.accept_order, name='accept_order'),
 ]
--- docicome/account/views.py
+++ docicome/account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from .forms import AccountCreationForm
 
+
 def register(request):
     form = AccountCreationForm()
 
@@ -10,6 +11,28 @@ def register(request):
         if form.is_valid():
             form.save()
 
-    context = {"form" : form }
+    context = {"form": form}
 
     return render(request, 'account/register.html', context)
+
+
+def expertise_orders_list(request):
+    # doctor = request.auth.doctor
+    # expertise = doctor.expertise
+    # orders_list = filter(orders.all, expertise=expertise)
+    orders_list = [
+        {'id': 1, 'patient': {'name': 'نام بیمار اول'},
+         'address': 'آدرس ۱', 'problem': 'شرح ۱'},
+        {'id': 2, 'patient': {'name': 'نام بیمار دوم'},
+         'address': 'آدرس ۲', 'problem': 'شرح ۲'},
+        {'id': 3, 'patient': {'name': 'نام بیمار سوم'},
+         'address': 'آدرس ۳', 'problem': 'شرح ۳'},
+    ]
+    return render(request, 'expertise_orders_list.html', {'orders_list': orders_list})
+
+
+def accept_order(request, order_id):
+    # if is doctor and same expertise and order is not taken by others
+    order = {'id': 1, 'patient': {'name': 'نام بیمار اول'},
+           'address': 'آدرس ۱', 'problem': 'شرح ۱'}
+    return render(request, 'accept_order.html', {'order': order})
