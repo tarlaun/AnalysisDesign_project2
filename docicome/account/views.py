04ed512e57e3a7894408ccbd5055eb4fbae4951d
negin <neginjfi79@gmail.com>
2021-11-17 14:01:31 +0330

    Expertise View added #51

--- docicome/account/views.py
+++ docicome/account/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .forms import AccountCreationForm
+from .models import Expertise
 
 def register(request):
     form = AccountCreationForm()
@@ -13,3 +15,11 @@ def register(request):
     context = {"form" : form }
 
     return render(request, 'account/register.html', context)
+
+
+class ExpertiseView(generic.ListView):
+    template_name = 'exp_list.html'
+    context_object_name = 'expertise_list'
+
+    def get_queryset(self):
+        return Expertise.objects.all()
