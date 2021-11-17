e754bbc4c4e54fdb06230a9c62435b484b030099
negin <neginjfi79@gmail.com>
2021-11-17 10:26:57 +0330

    Expertise Model Added #36 #37 #44

--- docicome/account/admin.py
+++ docicome/account/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from .models import Account, Doctor
+from .models import Account, Doctor, Expertise
 
 
 @admin.register(Account)
 class AuthorAdmin(admin.ModelAdmin):
     fields = Account.get_fields()
     list_display = Account.get_fields()
-     
+
+
 @admin.register(Doctor)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = Doctor.get_fields()
-     
-# admin.site.register(Account)
+
+
+admin.site.register(Expertise)
--- docicome/account/models.py
+++ docicome/account/models.py
@@ -3,18 +3,22 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models.enums import IntegerChoices
 from django.core.validators import RegexValidator
 
+
 # Create your models here.
 
 class UserType(IntegerChoices):
     DOCTOR = 1,
     PATIENT = 2
-  
+
+
 class Account(AbstractUser):
     user_type = models.SmallIntegerField('user_type', choices=UserType.choices, default=UserType.PATIENT)
-    phone_regex = RegexValidator(regex=r'^+9\d{9,11}$', message="Phone number must be entered in the format: '+999999999'.")
-    phone_number = models.CharField(validators=[phone_regex], max_length=13, blank=True, null=False) # validators should be a list
-    # add additional fields in here
+    phone_regex = RegexValidator(regex=r'^+9\d{9,11}$',
+                                 message="Phone number must be entered in the format: '+999999999'.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=13, blank=True,
+                                    null=False)  # validators should be a list
 
+    # add additional fields in here
 
     def __str__(self):
         return self.username
@@ -28,10 +32,18 @@ class Account(AbstractUser):
         return ('username', 'email', 'first_name', 'last_name', 'phone_number')
 
 
+class Expertise(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
 class Doctor(models.Model):
-    specialty = models.TextField()
+    expertise = models.ForeignKey(Expertise, on_delete=models.PROTECT)
     user = models.OneToOneField(Account, on_delete=models.CASCADE, primary_key=True)
-    
+
     @staticmethod
     def get_fields():
         return 'user', 'specialty'
