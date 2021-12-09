77577db58d3e7eb94ff1d423adcaa09deae1f0d6
mahsa.ama <mahsa.ama1391@gmail.com>
2021-12-09 19:04:19 +0330

    Dialog box for scoring added

---
+++ docicome/static/js/pop-up.js
@@ -0,0 +1,14 @@
+var updateButton = document.getElementById('Score');
+var favDialog = document.getElementById('favDialog');
+var outputBox = document.querySelector('output');
+var selectEl = document.querySelector('select');
+var confirmBtn = document.getElementById('confirmBtn');
+
+// "Score" button opens the <dialog> modally
+updateButton.addEventListener('click', function onOpen() {
+  if (typeof favDialog.showModal === "function") {
+    favDialog.showModal();
+  } else {
+    alert("The <dialog> API is not supported by this browser");
+  }
+});
