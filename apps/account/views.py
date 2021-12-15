3db40c247c42993fd59e35fd71c2866c0034fc35
Tarlan <babalgoogool@gmail.com>
2021-12-15 10:41:43 +0330

    Added the html page, minor fixes applied as well...

--- apps/account/views.py
+++ apps/account/views.py
@@ -152,4 +152,4 @@ def doctor_list(request, exp_id):
 
     expertise = get_object_or_404(Expertise, pk=exp_id)
     docs_list = Doctor.objects.filter(expertise=expertise)
-    return render(request, 'account/doctor_list.html', {'doctor_list': docs_list})
+    return render(request, 'account/doctor_list.html', {'doctor_list': docs_list, 'exp_name':expertise.name})
