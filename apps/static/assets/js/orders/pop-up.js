750d0a3ee0182fed487534b87729049afc488cb5
mahsa.ama <mahsa.ama1391@gmail.com>
2021-12-17 00:11:11 +0330

    some minor debugs in spell and comments

--- apps/static/assets/js/orders/pop-up.js
+++ apps/static/assets/js/orders/pop-up.js
@@ -1,11 +1,7 @@
-var updateButton = document.getElementById("Score");
 var favDialog = document.getElementById("favDialog");
-var outputBox = document.querySelector("output");
-var selectEl = document.querySelector("select");
-var confirmBtn = document.getElementById("confirmBtn");
 
 // "Score" button opens the <dialog> modally
-updateButton.addEventListener("click", function onOpen() {
+$(".Score").click(function onOpen() {
   if (typeof favDialog.showModal === "function") {
     favDialog.showModal();
   } else {
