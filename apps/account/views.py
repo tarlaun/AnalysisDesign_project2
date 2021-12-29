dbdfe3676929f52aeeef83b612fb2bd6cc8dcbea
Tarlan <babalgoogool@gmail.com>
2021-12-29 19:34:37 +0330

    Button successfully navigates to doctor_profile html page

--- apps/account/views.py
+++ apps/account/views.py
@@ -196,3 +196,10 @@ def previous_orders(request):
 def all_doctors(request):
     doctors_list = Doctor.objects.all()
     return render(request, 'account/list_of_doctors.html', {'doctors_list': doctors_list})
+
+def doc_pro(request, doc_id):
+    doctor = get_object_or_404(Doctor, pk=doc_id)
+    exp = doctor.expertise.name
+    return render(request, 'account/doctor_profile.html',
+                  {'doc_name': doctor.user.first_name, 'doc_lname': doctor.user.last_name, 'doc_id': doctor.user.id,
+                   'exp': exp})
