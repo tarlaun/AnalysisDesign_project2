8fd9058021c9f775404c4893accf6e77a15a7ffc
bahar <baharkh127@gmail.com>
2022-02-22 12:45:48 +0330

    changed help page design

--- apps/static/assets/js/help.js
+++ apps/static/assets/js/help.js
@@ -1,16 +1,8 @@
-var faq = document.getElementsByClassName("faq-page");
-var i;
-for (i = 0; i < faq.length; i++) {
-  faq[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-    this.classList.toggle("active");
-    /* Toggle between hiding and showing the active panel */
-    var body = this.nextElementSibling;
-    if (body.style.display === "block") {
-      body.style.display = "none";
-    } else {
-      body.style.display = "block";
-    }
-  });
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion() {
+  this.classList.toggle("active");
+  this.nextElementSibling.classList.toggle("active");
 }
+
+items.forEach(item => item.addEventListener("click", toggleAccordion));
